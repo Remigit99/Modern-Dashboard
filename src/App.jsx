c3752d1103c279b10fb14834b38107530dd31bf7
Remigit99 <aderemibiodun1000@gmail.com>
2023-04-26 11:37:56 +0100

    add page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import { useState } from 'react'
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import List from './Pages/List/List'
+import Single from './Pages/Single/Single'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
 
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<Home />} />
+            <Route path='login' element={<Login />} />
+
+            <Route path='users'>
+              <Route index element={<List />} />
+              <Route path=':userid' element={<Single />} />
+              <Route path='new' element={<New />} />
+            </Route>
+
+
+            <Route path='products'>
+              <Route index element={<List />} />
+              <Route path=':productid' element={<Single />} />
+              <Route path='new' element={<New />} />
+            </Route>
+
+          </Route>
+        </Routes>
+
+      </BrowserRouter>
+
     </div>
   )
 }
---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+    return (
+        <div>Login</div>
+    )
+
+}
+
+export default Login
